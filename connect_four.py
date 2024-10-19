00b7072e8deb29455e8bd39ce0112708e7ce8905
baconbub <zacharycoe94@gmail.com>
2024-10-19 14:08:39 -0500

    First commit.

---
+++ connect_four.py
@@ -0,0 +1,15 @@
+"""
+Game Name: Connect Four
+Description: Get four in a row to win!
+Author: Zachary Coe
+Date: 2024-10-16
+"""
+
+
+
+import random
+
+
+
+RED_CIRCLE = "\U0001f534"
+YELLOW_CIRCLE = "\U0001F7E1"
