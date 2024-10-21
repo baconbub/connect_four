64cb7f3c9880866b5394d678f6fc172895e81223
baconbub <zacharycoe94@gmail.com>
2024-10-21 06:31:05 -0500

    Hard mode added. Game also shows arrow pointing to previous player's move.

--- connect_four.py
+++ connect_four.py
@@ -8,6 +8,7 @@ Date: 2024-10-16
 
 
 import random
+from math import inf
 import argparse
 
 
@@ -16,12 +17,21 @@ DEFAULT_HEIGHT = 6
 DEFAULT_WIDTH = 7
 RED_CIRCLE = "\U0001f534"
 YELLOW_CIRCLE = "\U0001F7E1"
+POINTER = "↓"
 FOUR = 4
+THREE = 3
+TWO = 2
+TWO_ROW_SCORE = 100
+THREE_ROW_SCORE = 250
+BLOCK_SCORE = 300
 # Length of longer line in instructions
 SPACE_OVER = 44
 COLUMN_WIDTH = 6
 DOUBLE_DIGIT_INT = 10
 EMPTY_SPACE = " "
+WINNER_SCORE = 1000
+LOSER_SCORE = -1000
+MAX_DEPTH = 3
 
 
 
@@ -38,6 +48,12 @@ class Board:
         for row in range(self.height - 1, -1, -1):
             if self.board_spots[row][column] == EMPTY_SPACE:
                 self.board_spots[row][column] = player.color
+                return
+            
+    def undo_update(self, column):
+        for row in range(self.height):
+            if self.board_spots[row][column] != EMPTY_SPACE:
+                self.board_spots[row][column] = EMPTY_SPACE
                 return
 
     def get_spacing(self):
@@ -76,8 +92,18 @@ class Board:
 
         self.print_board_bottom()
 
-    def print_current_board(self):
+    def print_chosen_row(self, column):
+        print(" " * self.spacing, end="")
+        for col in range(self.width):
+            if col == column:
+                print(f"   {POINTER}  ", end="")
+            else:
+                print("      ", end="")
         print()
+
+    def print_current_board(self, column):
+        print()
+        self.print_chosen_row(column)
         for row in range(self.height - 1):
             print(" " * self.spacing, end="")
             for col in range(self.width):
@@ -112,7 +138,6 @@ class Board:
     
     def is_board_full(self):
         return all(all(col is not EMPTY_SPACE for col in row) for row in self.board_spots)
-            
 
 class Player:
     def __init__(self, color):
@@ -130,16 +155,12 @@ class Computer(Player):
         return "The computer"
 
 class Game:
-    instructions = """\n  Type the column number you want to drop
-your piece into. Get 4 in a row and you win!\n"""
-
     def __init__(self, height, width, hard=False):
         self.play_again = True
-        self.is_winner = False
         self.winner = None
         self.board = Board(height, width)
         self.intro_print()
-        self.hard = hard
+        self.hard = self.get_difficulty()
         self.player = Player(self.pick_piece())
         self.computer = Computer(self.set_computer_piece())
         self.games = 0
@@ -152,7 +173,6 @@ your piece into. Get 4 in a row and you win!\n"""
         print("\n" + (" " * first_spacing) + "Type the column number you want to drop")
         print((" " * second_spacing) + "your piece into. Get 4 in a row and you win!\n")
         
-
     def intro_print(self):
         # Prints information following instructions at launch
         self.print_instructions()
@@ -164,10 +184,22 @@ your piece into. Get 4 in a row and you win!\n"""
 
     def reset_game(self):
         self.reset_board()
-        self.is_winner = False
         self.winner = None
-        
 
+    def get_difficulty(self):
+        while True:
+            try:
+                options = ["easy", "e", "hard", "h"]
+                difficulty = input("\nSelect the difficulty (Easy/Hard): ").lower()
+                if difficulty in options[:2]:
+                    return False
+                elif difficulty in options[2:]:
+                    return True
+                else:
+                    raise ValueError()
+            except ValueError:
+                print("Invalid input. Please type 'easy' or 'hard'.")
+        
     def get_play_again(self):
         play_list = ["n", "y"]
         while True:     
@@ -236,12 +268,18 @@ your piece into. Get 4 in a row and you win!\n"""
         turn = random.choice(empty_columns)
         pause()
         self.board.update_board(turn, self.computer)
-        self.board.print_current_board()
+        self.board.print_current_board(turn)
+
+    def hard_computer_turn(self):
+        turn = self.find_best_move()
+        pause()
+        self.board.update_board(turn, self.computer)
+        self.board.print_current_board(turn)
     
     def player_turn(self):
         turn = self.get_player_turn()
         self.board.update_board(turn, self.player)
-        self.board.print_current_board()
+        self.board.print_current_board(turn)
     
     def player_start(self):
         self.player_turn()
@@ -252,10 +290,18 @@ your piece into. Get 4 in a row and you win!\n"""
             self.easy_computer_turn()
             if self.check_game_over():
                 return
+        else:
+            self.hard_computer_turn()
+            if self.check_game_over():
+                return
 
     def computer_start(self):
         if not self.hard:
             self.easy_computer_turn()
+            if self.check_game_over():
+                return
+        else:
+            self.hard_computer_turn()
             if self.check_game_over():
                 return
         
@@ -264,31 +310,28 @@ your piece into. Get 4 in a row and you win!\n"""
             return
 
     def check_game_over(self):
-        return self.check_for_winner() or self.board.is_board_full()
+        return self.check_for_winner()[0] or self.board.is_board_full()
 
     def check_for_winner(self):
         horizontal_win, winner = self.check_horizontal()
         if horizontal_win:
-            self.is_winner, self.winner = True, winner
-            return self.is_winner
+            return True, winner
 
         vertical_win, winner = self.check_vertical()
         if vertical_win:
-            self.is_winner, self.winner = True, winner
-            return self.is_winner
+            return True, winner
 
         rising_diagonal_win, winner = self.check_rising_diagonal()
         if rising_diagonal_win:
-            self.is_winner, self.winner = True, winner
-            return self.is_winner
+            return True, winner
 
         falling_diagonal_win, winner = self.check_falling_diagonal()
         if falling_diagonal_win:
-            self.is_winner, self.winner = True, winner
-            return self.is_winner
+            return True, winner
+        
+        return False, None
 
-
-    def check_horizontal(self):
+    def check_horizontal(self, in_a_row=FOUR):
         spots = self.board.board_spots
         for row in range(self.board.height):
             red_check = 0
@@ -303,13 +346,13 @@ your piece into. Get 4 in a row and you win!\n"""
                 else:
                     red_check = 0
                     yellow_check = 0
-                if red_check >= FOUR:
+                if red_check >= in_a_row:
                     return True, RED_CIRCLE
-                elif yellow_check >= FOUR:
+                elif yellow_check >= in_a_row:
                     return True, YELLOW_CIRCLE
         return False, None
     
-    def check_vertical(self):
+    def check_vertical(self, in_a_row=FOUR):
         spots = self.board.board_spots
         for col in range(self.board.width):
             red_check = 0
@@ -324,51 +367,191 @@ your piece into. Get 4 in a row and you win!\n"""
                 else:
                     red_check = 0
                     yellow_check = 0
-                if red_check >= FOUR :
+                if red_check >= in_a_row :
                     return True, RED_CIRCLE
-                elif yellow_check >= FOUR:
+                elif yellow_check >= in_a_row:
                     return True, YELLOW_CIRCLE
         return False, None
     
-    def check_rising_diagonal(self):
+    def check_rising_diagonal(self, in_a_row=FOUR):
         spots = self.board.board_spots
-        for row in range(self.board.height - 1, 2, -1):
-            for col in range(self.board.width - 3):
-                if (spots[row][col] == RED_CIRCLE and \
-                    spots[row - 1][col + 1] == RED_CIRCLE and \
-                        spots[row - 2][col + 2] == RED_CIRCLE and \
-                            spots[row - 3][col + 3] == RED_CIRCLE):
+        for row in range(self.board.height - 1, in_a_row - 2, -1):
+            for col in range(self.board.width - in_a_row + 1):
+                if all(spots[row - n][col + n] == RED_CIRCLE for n in range(in_a_row)):
                     return True, RED_CIRCLE
-                elif (spots[row][col] == YELLOW_CIRCLE and \
-                    spots[row - 1][col + 1] == YELLOW_CIRCLE and \
-                        spots[row - 2][col + 2] == YELLOW_CIRCLE and \
-                            spots[row - 3][col + 3] == YELLOW_CIRCLE):
+                elif all(spots[row - n][col + n] == YELLOW_CIRCLE for n in range(in_a_row)):
                     return True, YELLOW_CIRCLE
         return False, None
     
-    def check_falling_diagonal(self):
+    def check_falling_diagonal(self, in_a_row=FOUR):
         spots = self.board.board_spots
-        for row in range(FOUR - 1, self.board.height - 3):
-            for col in range(self.board.width - 3):
-                if (spots[row][col] == RED_CIRCLE and \
-                    spots[row - 1][col + 1] == RED_CIRCLE and \
-                        spots[row - 2][col + 2] == RED_CIRCLE and \
-                            spots[row - 3][col + 3] == RED_CIRCLE):
+        for row in range(self.board.height - in_a_row + 1):
+            for col in range(self.board.width - in_a_row + 1):
+                if all(spots[row + n][col + n] == RED_CIRCLE for n in range(in_a_row)):
                     return True, RED_CIRCLE
-                elif (spots[row][col] == YELLOW_CIRCLE and \
-                    spots[row - 1][col + 1] == YELLOW_CIRCLE and \
-                        spots[row - 2][col + 2] == YELLOW_CIRCLE and \
-                            spots[row - 3][col + 3] == YELLOW_CIRCLE):
+                elif all(spots[row + n][col + n] == YELLOW_CIRCLE for n in range(in_a_row)):
                     return True, YELLOW_CIRCLE
         return False, None
+    
+    def three_in_a_row_computer(self):
+        score = 0
+
+        if self.check_horizontal(THREE) == (True, self.computer.color):
+            score += THREE_ROW_SCORE
+        if self.check_vertical(THREE) == (True, self.computer.color):
+            score += THREE_ROW_SCORE
+        if self.check_rising_diagonal(THREE) == (True, self.computer.color):
+            score += THREE_ROW_SCORE
+        if self.check_falling_diagonal(THREE) == (True, self.computer.color):
+            score += THREE_ROW_SCORE
+
+        return score
+    
+    def two_in_a_row_computer(self):
+        score = 0
+
+        if self.check_horizontal(TWO) == (True, self.computer.color):
+            score += TWO_ROW_SCORE
+        if self.check_vertical(TWO) == (True, self.computer.color):
+            score += TWO_ROW_SCORE
+        if self.check_rising_diagonal(TWO) == (True, self.computer.color):
+            score += TWO_ROW_SCORE
+        if self.check_falling_diagonal(TWO) == (True, self.computer.color):
+            score += TWO_ROW_SCORE
+
+        return score
+
+    def three_in_a_row_player(self):
+        score = 0
+
+        if self.check_horizontal(THREE) == (True, self.player.color):
+            score -= THREE_ROW_SCORE
+        if self.check_vertical(THREE) == (True, self.player.color):
+            score -= THREE_ROW_SCORE
+        if self.check_rising_diagonal(THREE) == (True, self.player.color):
+            score -= THREE_ROW_SCORE
+        if self.check_falling_diagonal(THREE) == (True, self.player.color):
+            score -= THREE_ROW_SCORE
+
+        return score
+    
+    def two_in_a_row_player(self):
+        score = 0
+
+        if self.check_horizontal(TWO) == (True, self.player.color):
+            score -= TWO_ROW_SCORE
+        if self.check_vertical(TWO) == (True, self.player.color):
+            score -= TWO_ROW_SCORE
+        if self.check_rising_diagonal(TWO) == (True, self.player.color):
+            score -= TWO_ROW_SCORE
+        if self.check_falling_diagonal(TWO) == (True, self.player.color):
+            score -= TWO_ROW_SCORE
+
+        return score
+    
+    def check_for_blocks(self):
+        score = 0
+        empty_columns = self.board.get_empty_columns()
+
+        for col in empty_columns:
+            self.board.update_board(col, self.player)
+            
+            is_winner, winner = self.check_for_winner()
+
+            if is_winner and winner == self.player.color:
+                score -= BLOCK_SCORE
+
+            self.board.undo_update(col)
+        
+        return score
+    
+    def evaluation_function(self):
+        score = 0
+
+        # Three in a row
+        score += self.three_in_a_row_computer()
+        score += self.three_in_a_row_player()
+
+        # Two in a row
+        score += self.two_in_a_row_computer()
+        score += self.two_in_a_row_player()
+
+        score += self.check_for_blocks()
+        return score
+    
+    def minimax(self, is_maximizing=False, depth=0, alpha=(-inf), beta=(inf)):
+        is_winner, winner = self.check_for_winner()
+        if is_winner:
+            if winner == self.computer.color:
+                return WINNER_SCORE
+            elif winner == self.player.color:
+                return LOSER_SCORE
+        
+        # A tie
+        if self.board.is_board_full():
+            return 0
+        # Reached max recursion depth
+        if depth == MAX_DEPTH:
+            return self.evaluation_function()
+        
+        empty_columns = self.board.get_empty_columns()
+        
+        if is_maximizing:
+            best_score = float(-inf)
+            for col in range(self.board.width):
+                if col in empty_columns:
+                    self.board.update_board(col, self.computer)
+                    new_score = self.minimax(not is_maximizing, depth+1, alpha, beta)
+                    self.board.undo_update(col)
+                    best_score = max(best_score, new_score)
+                    alpha = max(alpha, best_score)
+                    if beta <= alpha:
+                        break
+            return best_score
+        
+        else:
+            best_score = float(inf)
+            for col in range(self.board.width):
+                    if col in empty_columns:
+                        self.board.update_board(col, self.player)
+                        new_score = self.minimax(not is_maximizing, depth+1, alpha, beta)
+                        self.board.undo_update(col)
+                        best_score = min(best_score, new_score)
+                        beta = min(beta, best_score)
+                        if beta <= alpha:
+                            break
+            return best_score
+        
+    def find_best_move(self):
+        best_score = (-inf)
+        best_move = -1
+        empty_columns = self.board.get_empty_columns()
+
+        for col in range(self.board.width):
+            if col in empty_columns:
+                self.board.update_board(col, self.computer)
+
+                current_score = self.minimax()
+
+                self.board.undo_update(col)
+
+                if current_score > best_score:
+                    best_move = col
+                    best_score = current_score
+
+        return best_move
+    
+    def set_winner(self):
+        winner = self.check_for_winner()[1]
+        self.winner = winner
     
     def update_scores(self):
         if self.player.color == self.winner:
             print(f"{self.player} win!!")
             self.player.score += 1
-        elif self.computer == self.winner:
+        elif self.computer.color == self.winner:
             print(f"{self.computer} wins!!")
-            self.computer.color += 1
+            self.computer.score += 1
     
     def print_scores(self):
         print(f"Wins:   {self.player.score}")
@@ -377,15 +560,13 @@ your piece into. Get 4 in a row and you win!\n"""
     
     def end_game(self):
         self.games += 1
+        self.set_winner()
         self.update_scores()
         self.print_scores()
         print()
         self.get_play_again()
         print()
                     
-
-        
-
 
 
 def parse_args():
@@ -428,10 +609,6 @@ def main():
         connect_four.end_game()
 
     
-
-    
-        
-
 
 if __name__ == "__main__":
     main()
